d94c2604b4044027d94f8bf7d8fe9ded85fbf98b
quentingruber <quentingruber@gmail.com>
2023-05-17 22:05:51 +0200

    more example

--- ts_example/test.ts
+++ ts_example/test.ts
@@ -8,6 +8,10 @@ function concat(a: string, b: string): string {
   return a + b;
 }
 
+export function concat2(a: string, b: string): string {
+  return a + b;
+}
+
 class classo {
   name: string;
   constructor(name: string) {
@@ -18,3 +22,22 @@ class classo {
     return name.toLowerCase();
   }
 }
+
+export class classo2 {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  toLower(name: string): string {
+    return name.toLowerCase();
+  }
+}
+
+const concat3 = (a: string, b: string): string => {
+  return a + b;
+};
+
+(a: string, b: string): string => {
+  return a + b;
+};
