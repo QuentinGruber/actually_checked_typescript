4bbbe8a22d6cd43ec035ac0bd67a2e3fbb2fc882
quentingruber <quentingruber@gmail.com>
2023-05-17 23:10:35 +0200

    name threads

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use act_lib::{act_process::process_file, args_parser::ActArgs};
 use clap::Parser;
-use std::{fs, path::PathBuf, thread::spawn};
+use std::{
+    fs,
+    path::PathBuf,
+    thread::{self, spawn},
+};
 
 fn get_files_paths(folder_path: String) -> Vec<PathBuf> {
     let files = fs::read_dir(folder_path)
@@ -22,9 +26,11 @@ fn main() {
     let folder_path = args.folder_path;
     let files = get_files_paths(folder_path);
     for file_path in files {
-        spawn(move || process_file(file_path).unwrap_or(()))
-            .join()
-            .unwrap_or(());
+        thread::Builder::new()
+            .name(file_path.to_string_lossy().to_string())
+            .spawn(move || process_file(file_path).unwrap_or(()))
+            .unwrap()
+            .join();
     }
 }
 
